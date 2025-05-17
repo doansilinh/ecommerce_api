39a19810d2864b359c8d6101646f06eeca82ed24
Doan Si Linh <doansilinh04@gmail.com>
2025-05-17 18:05:07 +0700

    update root api

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ from app.api import router_v1
 app = FastAPI(title="FastAPI Ecommerce", version="1.0.0")
 
 app.include_router(router_v1, prefix="/api")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my FastAPI app!"}
